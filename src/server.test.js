552c728f1c06d33c2d900d63ca15f1dc47deeb1e
Aaroneld <merrifield48@gmail.com>
2020-04-16 13:47:40 -0400

    built out initial server unit tests using jest/superagent

---
+++ src/server.test.js
@@ -0,0 +1,34 @@
+const server = require('./server');
+const request = require('supertest');
+
+describe("example test", () => {
+
+    test("it should work", () => {
+        expect(1).toBe(1);
+    })
+})
+
+describe("Test Apollo Server Initial Functionality", () => {
+    
+    let status;
+
+    beforeEach( async () => {
+        status = await  request(server)
+                        .post('/graphql')
+                        .send({query: `query { status }`});
+
+                
+    })
+
+    test('It returns 200 status', () => {
+
+        expect(status.status).toBe(200);
+    })
+
+    test('it returns an object whose value matches the expected object', () => {
+
+        const expected = {status: "Apollo Server is Running!"};
+
+        expect(status.body.data).toMatchObject(expected);
+    })
+})
