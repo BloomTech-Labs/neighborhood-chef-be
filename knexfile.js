55061ded90f3f856a977c98f14d51880355c5f94
Paul M Edwards <pauledwards@gmail.com>
2020-04-29 21:41:54 -0700

    Move .env.example & index.js to root; Update path to server.js within index.js; Add dotenv-safe to knexfile.js; Move knexfile testing entry to top & correct connection.

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,22 @@
+require('dotenv-safe').config({
+  allowEmptyValues: true
+});
+
 module.exports = {
+  testing: {
+    client: 'pg',
+    connection: process.env.POSTGRES_CONNECTION_STRING,
+    pool: {
+      min: 0,
+      max: 2,
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+
   development: {
     client: 'pg',
     connection: process.env.POSTGRES_CONNECTION_STRING,
@@ -39,19 +57,5 @@ module.exports = {
       tableName: 'knex_migrations',
     },
     // seeds: { directory: './data/seeds' },
-  },
-  testing: {
-    client: 'pg',
-    // has trouble connecting to postgres without template literal in testing mode
-    connection: `${process.env.POSTGRES_CONNECTION_STRING}`,
-    pool: {
-      min: 0,
-      max: 2,
-    },
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'knex_migrations',
-    },
-    seeds: { directory: './data/seeds' },
   },
 };
