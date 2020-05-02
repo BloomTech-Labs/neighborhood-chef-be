d036435811f117bd115b53841935450468ec1396
Paul M Edwards <pauledwards@gmail.com>
2020-05-01 20:46:43 -0700

    Revert knexfile changes to reduce this PR's scope.

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,11 @@
-const dotenvsafe = require('dotenv-safe');
-if (dotenvsafe.config) {
-  dotenvsafe.config({
-    allowEmptyValues: true
-  });
-}
+require('dotenv-safe').config({
+  allowEmptyValues: true
+});
 
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.POSTGRES_CONNECTION_STRING || 'postgres://neighborhoodchef:J4mb4l4y4!@localhost:5432/neighborhoodchef_dev',
+    connection: process.env.POSTGRES_CONNECTION_STRING,
     pool: {
       min: 0,
       max: 2,
