9d26e72da4e28f1e78260950070ded9fe94e1e8b
patrick-replogle <patrickr1138@gmail.com>
2020-05-14 19:11:17 -0700

    took out notNullable on the dropdown on the password column in migration 2 and changed Password to password to fit schema

--- data/migrations/20200429223718_alter-users-table-for-registration.js
+++ data/migrations/20200429223718_alter-users-table-for-registration.js
@@ -1,17 +1,16 @@
 
-exports.up = function(knex) {
-   return knex.schema.alterTable('Users', function(t) {
-        t.boolean('Activated')
-        .notNullable()
-        .defaultTo(false);
-        t.dropColumn('Password');
-      })
+exports.up = function (knex) {
+  return knex.schema.alterTable('Users', function (t) {
+    t.boolean('Activated')
+      .notNullable()
+      .defaultTo(false);
+    t.dropColumn('password');
+  })
 };
 
-exports.down = function(knex) {
-  return knex.schema.alterTable('Users', function(t) {
+exports.down = function (knex) {
+  return knex.schema.alterTable('Users', function (t) {
     t.dropColumn('Activated');
-    t.string('Password', 128)
-    .notNullable();
+    t.string('password', 128)
   })
 };
