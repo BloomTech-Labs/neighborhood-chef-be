2008c2cda7f44dc0127874fa44f433b8fe1e8a89
Aaroneld <merrifield48@gmail.com>
2020-06-10 22:25:16 -0400

    Add migration tables from event-ingredients

---
+++ data/migrations/20200609223613_event_ingredients.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("Event_Ingredients", tbl => {
+    tbl.increments();
+    tbl.integer('event_id').notNullable().unsigned().references('Events.id');
+    tbl.string('description', 255).notNullable();
+    tbl.boolean('requested').notNullable().defaultTo(false);
+    tbl.integer('user_id').notNullable().unsigned().references('Users.id');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('Event_ingredients');
+};
